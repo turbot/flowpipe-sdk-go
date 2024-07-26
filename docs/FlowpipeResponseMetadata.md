1bfbceb323e84bbdf7963a91cb06eaf0f42744de
Victor Hadianto <victor@turbot.com>
2024-07-26 15:50:24 +1000

    Update

--- docs/FlowpipeResponseMetadata.md
+++ docs/FlowpipeResponseMetadata.md
@@ -10,6 +10,7 @@ Name | Type | Description | Notes
 **Pipeline** | Pointer to **string** |  | [optional] 
 **PipelineExecutionId** | Pointer to **string** |  | [optional] 
 **Status** | Pointer to **string** |  | [optional] 
+**Type** | Pointer to **string** |  | [optional] 
 
 ## Methods
 
@@ -179,6 +180,31 @@ SetStatus sets Status field to given value.
 `func (o *FlowpipeResponseMetadata) HasStatus() bool`
 
 HasStatus returns a boolean if a field has been set.
+
+### GetType
+
+`func (o *FlowpipeResponseMetadata) GetType() string`
+
+GetType returns the Type field if non-nil, zero value otherwise.
+
+### GetTypeOk
+
+`func (o *FlowpipeResponseMetadata) GetTypeOk() (*string, bool)`
+
+GetTypeOk returns a tuple with the Type field if it's non-nil, zero value otherwise
+and a boolean to check if the value has been set.
+
+### SetType
+
+`func (o *FlowpipeResponseMetadata) SetType(v string)`
+
+SetType sets Type field to given value.
+
+### HasType
+
+`func (o *FlowpipeResponseMetadata) HasType() bool`
+
+HasType returns a boolean if a field has been set.
 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
--- model_flowpipe_response_metadata.go
+++ model_flowpipe_response_metadata.go
@@ -26,6 +26,7 @@ type FlowpipeResponseMetadata struct {
 	Pipeline *string `json:"pipeline,omitempty"`
 	PipelineExecutionId *string `json:"pipeline_execution_id,omitempty"`
 	Status *string `json:"status,omitempty"`
+	Type *string `json:"type,omitempty"`
 }
 func (o FlowpipeResponseMetadata) GetResourceType() string {
 	return "FlowpipeResponseMetadata"
@@ -239,6 +240,38 @@ func (o *FlowpipeResponseMetadata) SetStatus(v string) {
 	o.Status = &v
 }
 
+// GetType returns the Type field value if set, zero value otherwise.
+func (o *FlowpipeResponseMetadata) GetType() string {
+	if o == nil || IsNil(o.Type) {
+		var ret string
+		return ret
+	}
+	return *o.Type
+}
+
+// GetTypeOk returns a tuple with the Type field value if set, nil otherwise
+// and a boolean to check if the value has been set.
+func (o *FlowpipeResponseMetadata) GetTypeOk() (*string, bool) {
+	if o == nil || IsNil(o.Type) {
+		return nil, false
+	}
+	return o.Type, true
+}
+
+// HasType returns a boolean if a field has been set.
+func (o *FlowpipeResponseMetadata) HasType() bool {
+	if o != nil && !IsNil(o.Type) {
+		return true
+	}
+
+	return false
+}
+
+// SetType gets a reference to the given string and assigns it to the Type field.
+func (o *FlowpipeResponseMetadata) SetType(v string) {
+	o.Type = &v
+}
+
 func (o FlowpipeResponseMetadata) MarshalJSON() ([]byte, error) {
 	toSerialize,err := o.ToMap()
 	if err != nil {
@@ -266,6 +299,9 @@ func (o FlowpipeResponseMetadata) ToMap() (map[string]interface{}, error) {
 	}
 	if !IsNil(o.Status) {
 		toSerialize["status"] = o.Status
+	}
+	if !IsNil(o.Type) {
+		toSerialize["type"] = o.Type
 	}
 	return toSerialize, nil
 }
